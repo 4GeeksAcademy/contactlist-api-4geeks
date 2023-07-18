49ef1c2a91b5fb04e0790cb73c093c2e1da72097
Aleperix <aleperixx@gmail.com>
2023-07-18 15:21:56 +0000

    Added model for contact, and all CRUDs

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, re
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Contact
+from sqlalchemy import and_
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +36,150 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+## START -- API POST -- START ##
+@app.route('/contacts', methods=['POST'])
+def create_one_contact():
+    # We verify that all the data necessary to create a contact can be obtained.
+    full_name = email = agenda_slug = address = phone = None
+    try:
+        full_name = request.json["full_name"]
+        email = request.json["email"]
+        agenda_slug = request.json["agenda_slug"]
+        address = request.json["address"]
+        phone = request.json["phone"]
+    except KeyError as e:
+        raise APIException(f"You need specify the contact's {e.args[0]}.", status_code=400)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # We run the different verifications, depending on the data we have.
+    values = {"full_name": full_name, "email": email, "agenda_slug": agenda_slug, "address": address, "phone": phone}
+    execute_verifications(values)
 
-    return jsonify(response_body), 200
+    # We create the new contact and return a message.
+    new_contact = Contact(full_name=full_name, email=email, agenda_slug=agenda_slug, address=address, phone=phone)
+
+    db.session.add(new_contact)
+    db.session.commit()
+    return jsonify({"message": f"Contact {full_name} is added succesfully to {agenda_slug} agenda."}), 200
+## END -- API POST -- END ##
+
+## START -- API GET -- START ##
+@app.route('/contacts/agendas', methods=['GET'])
+def get_agendas():
+    # We get all the agendas available so far.
+    agendas = Contact.query.with_entities(Contact.agenda_slug).all()
+    
+    if len(agendas) == 0:
+        raise APIException('Not found', status_code=404)
+    
+    results = list(map(lambda item: dict(item)["agenda_slug"], agendas))
+    return jsonify(results), 200
+
+@app.route('/contacts/agenda/<string:agenda_slug>', methods=['GET'])
+def get_contacts_from_agenda(agenda_slug):
+    # We obtain all the contacts of a specific agenda.
+    contacts = Contact.query.filter_by(agenda_slug=agenda_slug).all()
+    
+    if len(contacts) == 0:
+        raise APIException(f'No contacts found in the {agenda_slug} agenda.', status_code=404)
+    
+    results = list(map(lambda item: item.serialize(), contacts))
+    return jsonify(results), 200
+
+@app.route('/contacts/<int:contact_id>', methods=['GET'])
+def get_one_contact_by_id(contact_id):
+    # We obtain a contact with specific id.
+    contact = Contact.query.filter_by(id=contact_id).first()
+    
+    if contact is None:
+        raise APIException(f'The contact with the id {contact_id} was not found.', status_code=404)
+    
+    result = contact.serialize()
+    return jsonify(result), 200
+## END -- API GET -- END ##
+
+## START -- API PUT -- START ##
+@app.route('/contacts/<int:contact_id>', methods=['PUT'])
+def modify_one_contact_by_id(contact_id):
+    # We modify a contact with specific id.
+    body = request.json
+    contact = Contact.query.filter_by(id=contact_id).first()
+    
+    if contact is None:
+        raise APIException(f'The contact with the id {contact_id} was not found.', status_code=404)
+
+    execute_verifications(body)
+    for key in body:
+        for col in contact.serialize():
+            if key == col and key != "id":
+                setattr(contact, col, body[key])
+    
+    db.session.commit()
+
+    return jsonify({"message": f"The contact with the id {contact_id} modified succesfully."}), 200
+## END -- API PUT -- END ##
+
+## START -- API DELETE -- START ##
+@app.route('/contacts/<int:contact_id>', methods=['DELETE'])
+def delete_one_contact_by_id(contact_id):
+    # We delete a contact with specific id.
+    contact = Contact.query.filter_by(id=contact_id).first()
+    
+    if contact is None:
+        raise APIException(f'The contact with the id {contact_id} was not found.', status_code=404)
+
+    Contact.query.filter_by(id=contact_id).delete()
+    db.session.commit()
+
+    return jsonify({"message": f"The contact with the id {contact_id} deleted succesfully."}), 200
+## END -- API DELETE -- END ##
+
+
+def verify_regex(key, value, regex):
+    if not re.match(regex, value):
+        raise APIException(f'You need specify a valid {key}.', status_code=400)
+
+def verify_length(value, key, min, max):
+    if len(value) < min:
+        raise APIException(f"The minimum length for contact's {key} is {min} characters.", status_code=400)
+    
+    if len(value) > max:
+        raise APIException(f"The maximum length for contact's {key} is {max} characters.", status_code=400)
+
+
+def verify_many_empty(values):
+    for key in values:
+        if values[key] is None or values[key] == "":
+            raise APIException(f"The contact's {key} cannot be empty.", status_code=400)
+
+def verify_email_exist_in_agenda(agenda_slug, email):
+        contact = Contact.query.filter(and_(Contact.agenda_slug == agenda_slug, Contact.email == email)).first()
+        if contact:
+            raise APIException(f"The contact with the email {email} already exist in {agenda_slug} agenda.", status_code=403)
+
+def execute_verifications(values):
+    allowed_keys = ['full_name', 'email', 'agenda_slug', 'address', 'phone']
+    for key in values:
+        if key not in allowed_keys:
+            raise APIException(f"The {key} is not a valid key.", status_code=400)
+    
+    verify_many_empty(values)
+    
+    if 'email' in values and 'agenda_slug' in values:
+        verify_email_exist_in_agenda(values['agenda_slug'], values['email'])
+    
+    if 'email' in values:
+        email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        verify_regex('email address', values['email'], email_regex)
+    
+    if 'phone' in values:
+        phone_regex = r'^[0-9]+$'
+        verify_regex('phone number', values['phone'], phone_regex)
+        verify_length(values['phone'], 'phone', 3, 20)
+    
+    if 'agenda_slug' in values:
+        if re.search(r"\s", values['agenda_slug']):
+            raise APIException(f"The agenda_slug must not contain spaces.", status_code=400)
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    full_name = db.Column(db.String(150), unique=False, nullable=False)
+    email = db.Column(db.String(150), unique=False, nullable=False)
+    agenda_slug = db.Column(db.String(150), unique=False, nullable=False)
+    address = db.Column(db.String(150), unique=False, nullable=False)
+    phone = db.Column(db.String(20), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
+            "agenda_slug": self.agenda_slug,
+            "address": self.address,
+            "phone": self.phone
             # do not serialize the password, its a security breach
         }
